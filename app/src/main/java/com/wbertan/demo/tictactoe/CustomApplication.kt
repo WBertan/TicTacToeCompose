8a4dce1b6f9b6551855b577c34c3ff697f27fce8
WBertan <william.bertan@gmail.com>
2021-05-14 15:57:22 +0100

    Add Hilt as a test.

---
+++ app/src/main/java/com/wbertan/demo/tictactoe/CustomApplication.kt
@@ -0,0 +1,8 @@
+package com.wbertan.demo.tictactoe
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CustomApplication : Application() {
+}
--- app/src/main/java/com/wbertan/demo/tictactoe/MainActivity.kt
+++ app/src/main/java/com/wbertan/demo/tictactoe/MainActivity.kt
@@ -10,8 +10,41 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.wbertan.demo.tictactoe.game.SimpleGameView
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
 
+interface Repository {
+    val name: String
+}
+
+class RepositoryImpl @Inject constructor() : Repository {
+    override val name = "repository"
+}
+
+@HiltViewModel
+class DummyViewModel @Inject constructor(
+    repository: Repository
+) : ViewModel() {
+    val a = repository.name
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AnalyticsModule {
+
+    @Binds
+    abstract fun bind(impl: RepositoryImpl): Repository
+}
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +56,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun App() {
-    SimpleGameView.SimpleGame(Modifier.padding(8.dp))
-}
+    @Composable
+    fun App(dummyViewModel: DummyViewModel = viewModel()) {
+        println("asas ${dummyViewModel.a}")
+        SimpleGameView.SimpleGame(Modifier.padding(8.dp))
+    }
 
-@Preview(showBackground = true, backgroundColor = 0xFF03A9F4)
-@Composable
-fun PreviewApp() {
-    MaterialTheme {
-        App()
+    @Preview(showBackground = true, backgroundColor = 0xFF03A9F4)
+    @Composable
+    fun PreviewApp() {
+        MaterialTheme {
+            App()
+        }
     }
 }
